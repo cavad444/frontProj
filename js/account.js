f89179937946fe2ccb02bd929e4f2a7aec8f2c9d
rzamirzayev <rzatm@code.edu.az>
2023-04-11 23:40:19 +0400

    asdaf

---
+++ js/account.js
@@ -0,0 +1,21 @@
+// -----------------
+const accordionQuestionHeaders = document.querySelectorAll(
+  ".bc-section-question-box-top"
+);
+
+accordionQuestionHeaders.forEach((accordionQuestionHeader) => {
+  accordionQuestionHeader.addEventListener("click", () => {
+    console.log("salam");
+    accordionQuestionHeader.classList.toggle("active");
+    const accordionItemBody = accordionQuestionHeader.nextElementSibling;
+    const accordionItemQuestion = accordionQuestionHeader.firstChild;
+    console.log(accordionItemBody);
+    if (accordionQuestionHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+      accordionItemBody.style.marginTop = "30px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+      accordionItemBody.style.marginTop = "0";
+    }
+  });
+});
