bfd366b98ae8f0c0bbd8fbfc1316cf32dce9a6c7
rzamirzayev <rzatm@code.edu.az>
2023-04-13 15:20:58 +0400

    ada

---
+++ js/sif-loans.js
@@ -0,0 +1,17 @@
+const additionButtons = document.querySelectorAll(".sl-box-button");
+
+additionButtons.forEach((additionButton) => {
+  additionButton.addEventListener("click", () => {
+    additionButton.classList.toggle("active");
+    const accordionItemBody =
+      additionButton.parentNode.parentNode.parentNode.nextElementSibling;
+    console.log(accordionItemBody);
+    if (additionButton.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+      additionButton.innerHTML = "Bağla";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+      additionButton.innerHTML = "Ətraflı";
+    }
+  });
+});
